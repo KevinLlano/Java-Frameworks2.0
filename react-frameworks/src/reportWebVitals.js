a8f8d355aa171d51bffb849e700b9ab900d5c040
Kevin Llanos <155478857+KevinLlano@users.noreply.github.com>
2025-01-22 20:29:46 -0600

    Update reportWebVitals.js

--- react-frameworks/src/reportWebVitals.js
+++ react-frameworks/src/reportWebVitals.js
@@ -1,13 +1,19 @@
+// Defines a function to report web vital metrics
 const reportWebVitals = onPerfEntry => {
+  // Check if the provided argument is a function
   if (onPerfEntry && onPerfEntry instanceof Function) {
+    // Dynamically import the web-vitals library and destructure the metrics functions
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+      // Call each metric function and pass the onPerfEntry callback for handling the results
+      getCLS(onPerfEntry);  // Cumulative Layout Shift
+      getFID(onPerfEntry);  // First Input Delay
+      getFCP(onPerfEntry);  // First Contentful Paint
+      getLCP(onPerfEntry);  // Largest Contentful Paint
+      getTTFB(onPerfEntry); // Time to First Byte
     });
   }
 };
 
+
 export default reportWebVitals;
+
