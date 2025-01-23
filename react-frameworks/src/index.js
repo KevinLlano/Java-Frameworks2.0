451e6692cfa464721888da5f3f22ad1d74b082fd
Kevin Llanos <155478857+KevinLlano@users.noreply.github.com>
2025-01-22 20:25:03 -0600

    Update index.js

--- react-frameworks/src/index.js
+++ react-frameworks/src/index.js
@@ -1,3 +1,4 @@
+// Import necessary modules and components
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -6,16 +7,17 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+// Create a root element for rendering the React app
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component wrapped in Router and StrictMode
 root.render(
   <React.StrictMode>
     <Router>
-    <App />
+      <App />
     </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Measuring app performance for Browser
 reportWebVitals();
